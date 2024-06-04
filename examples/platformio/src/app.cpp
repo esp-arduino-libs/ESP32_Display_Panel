f1281a2cc281e9fa517893e8e85770e6fda84488
IT: Sop.Son <sop.son@feavn.local>
2024-06-04 13:22:37 +0700

    update

--- examples/platformio/src/app.cpp
+++ examples/platformio/src/app.cpp
@@ -1,11 +1,22 @@
+
 #include <Arduino.h>
 #include <ESP_Panel_Library.h>
 #include <lvgl.h>
-#include <demos/lv_demos.h>
 #include "lvgl_port_v8.h"
+
+/**
+/* To use the built-in examples and demos of LVGL uncomment the includes below respectively.
+ * You also need to copy `lvgl/examples` to `lvgl/src/examples`. Similarly for the demos `lvgl/demos` to `lvgl/src/demos`.
+ */
+// #include <demos/lv_demos.h>
+// #include <examples/lv_examples.h>
+
 void setup()
 {
+    String title = "LVGL porting example";
+
     Serial.begin(115200);
+    Serial.println(title + " start");
 
     Serial.println("Initialize panel device");
     ESP_Panel *panel = new ESP_Panel();
@@ -16,7 +27,6 @@ void setup()
     rgb_bus->configRgbFrameBufferNumber(LVGL_PORT_DISP_BUFFER_NUM);
     rgb_bus->configRgbBounceBufferSize(LVGL_PORT_RGB_BOUNCE_BUFFER_SIZE);
 #endif
-
     panel->begin();
 
     Serial.println("Initialize LVGL");
@@ -26,13 +36,35 @@ void setup()
     /* Lock the mutex due to the LVGL APIs are not thread-safe */
     lvgl_port_lock(-1);
 
-    lv_demo_music();
+    /* Create a simple label */
+    lv_obj_t *label = lv_label_create(lv_scr_act());
+    lv_label_set_text(label, title.c_str());
+    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+
+    /**
+     * Try an example. Don't forget to uncomment header.
+     * See all the examples online: https://docs.lvgl.io/master/examples.html
+     * source codes: https://github.com/lvgl/lvgl/tree/e7f88efa5853128bf871dde335c0ca8da9eb7731/examples
+     */
+    //  lv_example_btn_1();
+
+    /**
+     * Or try out a demo.
+     * Don't forget to uncomment header and enable the demos in `lv_conf.h`. E.g. `LV_USE_DEMOS_WIDGETS`
+     */
+    // lv_demo_widgets();
+    // lv_demo_benchmark();
+    // lv_demo_music();
+    // lv_demo_stress();
+
     /* Release the mutex */
     lvgl_port_unlock();
+
+    Serial.println(title + " end");
 }
 
 void loop()
 {
-
-    delay(0x10);
+    Serial.println("IDLE loop");
+    delay(1000);
 }
