92f5a254a3b44b3caf5c4fed0fc925f21626df50
Ludovic BOUÉ <lboue@users.noreply.github.com>
2024-07-08 12:06:07 +0200

    Init GT911 touch controller by the INT pin

--- src/board/elecrow/CROWPANEL_7_0.h
+++ src/board/elecrow/CROWPANEL_7_0.h
@@ -205,7 +205,15 @@
 // #define ESP_PANEL_BEGIN_START_FUNCTION( panel )
 // #define ESP_PANEL_BEGIN_EXPANDER_START_FUNCTION( panel )
 // #define ESP_PANEL_BEGIN_EXPANDER_END_FUNCTION( panel )
-// #define ESP_PANEL_BEGIN_LCD_START_FUNCTION( panel )
+#define ESP_PANEL_BEGIN_LCD_START_FUNCTION( panel )  \
+
+    {  \
+        /* Maintain the touch INT signal in a low state during the reset process to set its I2C address to `0x5D` */ \
+        gpio_set_direction((gpio_num_t)ESP_PANEL_TOUCH_IO_INT, GPIO_MODE_OUTPUT); \
+        gpio_set_level((gpio_num_t)ESP_PANEL_TOUCH_IO_INT, 0); \
+        vTaskDelay(pdMS_TO_TICKS(10)); \
+    }
+
 // #define ESP_PANEL_BEGIN_LCD_END_FUNCTION( panel )
 // #define ESP_PANEL_BEGIN_TOUCH_START_FUNCTION( panel )
 // #define ESP_PANEL_BEGIN_TOUCH_END_FUNCTION( panel )
